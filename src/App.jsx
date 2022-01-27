ff0cabb2a04bb7e4d9c23fee5c3db8d99e432877
Adam Gonzales <agonzales@tethertools.com>
2022-01-27 09:05:00 -0700

    feature: add graphic blob to DOM, style elements, add components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Question from './components/Question'
 import logo from './img/logo.svg'
 import './App.css'
 
 function App () {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    ;(async function () {
+      if (CSS.paintWorklet === undefined) {
+        await import('https://unpkg.com/css-paint-polyfill')
+      }
+
+      CSS.paintWorklet.addModule(
+        'https://unpkg.com/@georgedoescode/houdini-random-blobs'
+      )
+      document
+        .querySelector('.worklet-canvas')
+        .style.setProperty('--blob-seed', Math.random() * 10000)
+    })()
+  })
+
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className='app'>
+      <div className='quiz'>
+        <div className='worklet-canvas' />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
         <p>
           <button type='button' onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
-      </header>
+      </div>
     </div>
   )
 }
---
+++ src/components/Question.jsx
@@ -0,0 +1,13 @@
+export default function Question () {
+  return (
+    <div className='question'>
+      <h2>How would one say goodbye in Spanish?</h2>
+      <ul>
+        <li>Adios</li>
+        <li>Nada</li>
+        <li>Bueno</li>
+        <li>Alto</li>
+      </ul>
+    </div>
+  )
+}
